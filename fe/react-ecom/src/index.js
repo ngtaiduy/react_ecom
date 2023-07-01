9ddcd5e76c80988e51f805bfc595adb98bb9e14d
Tài Duy <duyntph12791@fpt.edu.vn>
2023-07-01 09:44:05 +0700

    up b2

--- fe/react-ecom/src/index.js
+++ fe/react-ecom/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import HomePage from "./pages/users/homePage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>TD</h1>);
+root.render(  <HomePage></HomePage> );
